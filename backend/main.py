07ed0aa190cec483e1ee5345554bf8de2cdff912
Matthew Tujague <matthewtujague@gmail.com>
2024-09-28 17:48:52 -0400

    func skeletons & docker

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,28 @@
 from flask import Flask
+from markupsafe import escape
+
+
 import jsonify, requests
+
 
 app = Flask(__name__)
 
-@app.route("/nextSong", methods=["GET"])
+@app.route("/redirect")
+def redundantRedirect():
+    #specifically for oauth on serverside spotify requests
+    return
+
+@app.route("/nextSong/int:<partyID>", methods=["GET"])
 def nextSong():
     pass
 
-@app.route("/queue", methods=["POST"])
+@app.route("/queue/<int:partyID>", methods=["GET"])
 def queue():
-    #takes a number N from post request
+    #takes a 3 songs from internal queue
+    pass
+
+#direct database interactions
+
+@app.route("/party/int:<partyID>", methods=["GET"])
+def queryParty():
+    pass
