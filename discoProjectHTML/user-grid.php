faac8896dc85125a96fb4edaf38fd5ccacf22be5
CapuchinoGaming <127352818+CapuchinoGaming@users.noreply.github.com>
2024-09-29 05:36:33 -0400

    discoProject1
    Created user webpage.

---
+++ discoProjectHTML/user-grid.php
@@ -0,0 +1,9 @@
+<!-- HTML grid for our "user" webpage -->
+<div class="user-grid">
+    <div class="borders"></div>
+    <div class="album_cover"><img src="plantImage.jpg" alt="Potted plants" height="100%" width="100%" object-fit="contain"></div>
+    <div class="song_name">Raining Tacos</div>
+    <div class="artist_name">By Parry Grip</div>
+    <div class="audio_slider">===0===========</div>
+    <div class="timestamp">0:00 / 2:23</div>
+</div>
---
+++ discoProjectHTML/user-results.php
@@ -0,0 +1,14 @@
+<!-- User webpage -->
+<html>
+    <head>
+        <title>Groove - usergrid</title>
+        <link rel="stylesheet" href="userstyle.css"/>
+    </head>
+    
+    <body>
+        <main>
+            <h3>User Grid:</h3>
+            <?php include('user-grid.php'); ?>
+        </main>
+    </body>
+</html>
