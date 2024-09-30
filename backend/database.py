f1b40270e5b37c10b47ae7d2cb1a1cb1b241efb5
Matthew Tujague <matthewtujague@gmail.com>
2024-09-30 17:13:07 -0400

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("github-actions@groove-437022.iam.gserviceaccount.com")
-app = firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app()
 cli = firestore.client()
 
 def decompose(obj):
